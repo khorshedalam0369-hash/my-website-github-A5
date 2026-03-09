e53380588c88265f8716993af12bf835ab96a3c5
khorshedalam0369-hash <khorshedalam0369@gmail.com>
2026-03-09 23:15:44 +0800

    modal Issues and load Search Issue

--- js/home.js
+++ js/home.js
@@ -17,6 +17,7 @@ document.getElementById('logOut').addEventListener('click',()=>{
   window.location.href="./index.html"
 })
 
+
 function showSpinner(status) {
   if (status === true) {
     spinner.classList.remove("hidden");
@@ -84,4 +85,141 @@ async function loadSingleIssues(id) {
   const data = await res.json();
   modalIssues(data.data);
   showSpinner(false);
+}
+
+async function modalIssues(data) {
+  const modal = document.getElementById("modal");
+  modal.innerHTML = "";
+  const createModal = document.createElement("div");
+  createModal.innerHTML = `
+  <div class="modal-box  w-5xl">
+                <h3 class="text-xl font-bold">${data.title}</h3>
+                <div class=" flex items-center gap-1 md:gap-3 mb-3">
+                <div><p class=" text-center text-white uppercase rounded-full px-2 text-sm md:text-lg ${data.status === "open" ? "bg-green-500" : "bg-purple-500"}"> ${data.status}</p></div>
+                    <p class="flex text-sm md:text-lg">Opened by <span>${data.author}.</span></p>
+                    <p class="flex text-sm md:text-lg"><span>${new Date(data.createdAt).toLocaleDateString("en-GB")}</span></p>
+                </div>
+
+                <div class="flex gap-2 my-3">
+                <div class="text-sm md:text-lg">
+                ${data.labels[0]?` <p class=" w-fit bg-red-100 rounded-full border border-red-500 text-red-500  px-1 font-bold uppercase ">${data.labels[0].toLowerCase()==='bug'? '<i class="fa-solid fa-bug"></i>':'<i class="fa-solid fa-wand-magic-sparkles"></i>' }
+                <span>${data.labels[0]}</span>
+              </p>`:''}
+              </div>
+              <div class="text-sm md:text-lg">
+              ${data.labels[1]?` <p class="w-fit  bg-yellow-100 rounded-full  border border-yellow-500 text-yellow-500 px-1  font-bold uppercase">
+                <span><i class="fa-regular fa-life-ring"></i>${data.labels[1]}</span>
+              </p>`:''}
+              </div>
+              </div>
+
+                <p class="">${data.description}</p>
+                <div class="flex justify-between my-3 bg-gray-100 rounded-lg p-4">
+                    <div>
+                    ${data.assignee ?`<p>assignee:</p>
+                      <p class="text-xl font-bold">${data.assignee}</p>`:''}
+                        
+                    </div>
+                    <div class="flex flex-col items-center ">
+                        <p>priority</p>
+                        <h1 class="${data.priority === "high" ? "bg-red-100 text-red-500" : data.priority === "medium" ? "bg-yellow-100 text-yellow-500" : "bg-gray-200 text-gray-500"}  px-2  rounded-full  font-bold uppercase">${data.priority}</h1>
+                    </div>
+                </div>
+                <div class="modal-action">
+                  <form method="dialog">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <button class="btn btn-error text-white  ">Close</button>
+                  </form>
+                </div>
+              </div>
+              `;
+  modal.append(createModal);
+  issues_modal.showModal();
+}
+
+async function loadSearchIssue() {
+  const searchInput = document.getElementById("search");
+  const searchValue = searchInput.value;
+  const selectedBtn = document.querySelectorAll("#buttons .btn");
+  selectedBtn.forEach((btn) => {
+    console.log(btn)
+    btn.classList.remove("btn-primary");
+  });
+  if (searchValue.trim() === "") {
+    document.getElementById('allBtn').classList.add('btn-primary')
+    loadAllIssues();
+    return;
+  }
+  showSpinner(true);
+  const res = await fetch(
+    `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchValue}`,
+  );
+  const data = await res.json();
+  displayAllIssues(data.data);
+  showSpinner(false);
+}
+document.getElementById("search").addEventListener("input", loadSearchIssue);
+
+loadAllIssues();
+
+async function displayAllIssues(cards) {
+  cardContainer.innerHTML = "";
+  openCards.innerHTML = "";
+  closedCards.innerHTML = "";
+  countCard = [];
+  countOpenCard = [];
+  countClosed = [];
+  cards.forEach((card) => {
+    countCard.push(card);
+    function newCardFunc() {
+      newCard = document.createElement("div");
+      newCard.innerHTML = `
+              <div onclick="loadSingleIssues(${card.id})" class="card card-body  shadow space-y-3 md:space-y-5 h-full border-t-6 ${card.status === "open" ? "border-green-500" : "border-purple-500"}">
+              <div class="flex justify-between">
+              ${card.status === "open" ? '<i class="fa-solid fa-circle-check text-green-500 text-2xl md:text-3xl"></i>' : '<i class="fa-regular fa-circle-check text-purple-500 text-2xl md:text-3xl"></i>'}
+              
+                <h1 class="${card.priority === "high" ? "bg-red-100 text-red-500" : card.priority === "medium" ? "bg-yellow-100 text-yellow-500" : "bg-gray-200 text-gray-500"} py-1 px-5  rounded-full  font-bold uppercase">${card.priority}</h1>
+              </div>
+              <div class="space-y-2">
+                <h1 class="text-2xl font-bold line-clamp-1">
+                  ${card.title}
+                </h1>
+                <p class="text-gray-400 line-clamp-2">
+                  ${card.description}
+                </p>
+              </div>
+              <div class="flex gap-2 ">
+              <div>
+              ${card.labels[0]?` <p class=" w-fit bg-red-100 rounded-full border border-red-500 text-red-500  p-1 font-bold uppercase ">${card.labels[0].toLowerCase()==='bug'? '<i class="fa-solid fa-bug"></i>':'<i class="fa-solid fa-wand-magic-sparkles"></i>' }
+                <span>${card.labels[0]}</span>
+              </p>`:''}
+              </div>
+              <div>
+              ${card.labels[1]?` <p class="w-fit  bg-yellow-100 rounded-full  border border-yellow-500 text-yellow-500 p-1  font-bold uppercase">
+                <span><i class="fa-regular fa-life-ring"></i>${card.labels[1]}</span>
+              </p>`:''}
+              </div>
+              </div>
+              <div>
+              </div>
+              <div class="divider"></div>
+              <div class="space-y-2">
+                <p>#1 by ${card.author}</p>
+                <p>${new Date(card.updatedAt).toLocaleDateString("en", "GB")}</p>
+              </div>
+            </div>
+              `;         
+      return newCard;
+    }
+    
+    if (card.status === "open") {
+      countOpenCard.push(card);
+      openCards.append(newCardFunc());
+    } else if (card.status === "closed") {
+      countClosed.push(card);
+      closedCards.append(newCardFunc());
+    }
+    cardContainer.append(newCardFunc());
+  });
+  updateCount("all");
 }
